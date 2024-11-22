91fbfc8e168c26384d95c9ba8cb0fdc10c02f2ed
igor-colonna <igorcolonna@gmail.com>
2024-11-22 12:01:18 -0300

    Aura Projectile Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,16 @@
+// Copyright Igorcolonna
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("Active Ability"), true, true, FLinearColor::Yellow, 3);
+	
+}
